2312cdf146d966d6635e266454bdd542dad9928b
Matheus Dias <matheusdias.front96@gmail.com>
2026-01-27 16:33:56 -0300

    feat(schema): refactor schema handling and introduce getSchema function

---
+++ lucrafood-api/src/kernel/decorators/Schema.ts
@@ -0,0 +1,14 @@
+import type z from 'zod';
+
+const SCHEMA_METADATA_KEY = 'custom:schema';
+
+export function Schema(schema: z.ZodObject): ClassDecorator {
+  return (target) => {
+    Reflect.defineMetadata(SCHEMA_METADATA_KEY, schema, target);
+  };
+}
+
+export function getSchema(target: any): z.ZodObject | undefined {
+  return Reflect.getMetadata(SCHEMA_METADATA_KEY, target.constructor);
+
+}
