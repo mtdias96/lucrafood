a3276fef4b50b131541a2a2678fcea62bfdf78ff
Matheus Dias <matheusdias.front96@gmail.com>
2026-01-27 16:32:38 -0300

    refactor(api): update authRoutes to use Registry for controller instantiation

--- lucrafood-api/src/application/routes/public/authRoutes.ts
+++ lucrafood-api/src/application/routes/public/authRoutes.ts
@@ -1,12 +1,13 @@
 
-import { SignInController } from '@/application/controllers/auth/SignInController';
-import { SignUpController } from '@/application/controllers/auth/SignUpController';
-import { fastifyRouteAdapter } from '@/main/adapters/fastifyRoute';
+import { SignInController } from '@application/controllers/auth/SignInController';
+import { SignUpController } from '@application/controllers/auth/SignUpController';
+import { Registry } from '@kernel/di/Registry';
+import { fastifyRouteAdapter } from '@main/adapters/fastifyRouteAdapter';
 import { FastifyPluginAsync } from 'fastify';
 
 export const authRoutes: FastifyPluginAsync = async (fastify) => {
-  const signUpController = new SignUpController();
-  const signInController = new SignInController();
+  const signUpController = Registry.getInstace().resolve(SignUpController);
+  const signInController = Registry.getInstace().resolve(SignInController);
 
   fastify.post('/signup', fastifyRouteAdapter(signUpController));
   fastify.post('/signin', fastifyRouteAdapter(signInController));
