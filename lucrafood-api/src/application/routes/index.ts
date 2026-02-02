ea0d720c985f00a546e250825fa644778ca3fe3e
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-02 09:09:22 -0300

    feat(api): remove unused authentication routes and database schema

--- lucrafood-api/src/application/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-import { authRoutes } from './public/authRoutes';
-
-export const routes: FastifyPluginAsync = async (fastify) => {
-  fastify.register(authRoutes, { prefix: 'auth' });
-};
--- lucrafood-api/src/infra/database/Schemas/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
-
-export const usersTable = pgTable('users', {
-  id: uuid().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password_hash: varchar().notNull(),
-});
