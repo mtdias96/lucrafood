0acf551707212038317b5f9a1a09044876b84915
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-02 11:42:30 -0300

    chore(api): alter path to schemas

--- lucrafood-api/drizzle.config.ts
+++ lucrafood-api/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/infra/database/schemas/index.ts',
+  schema: './src/infra/database/drizzle/schemas/index.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
