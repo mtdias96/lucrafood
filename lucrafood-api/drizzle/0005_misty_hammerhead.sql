67a3e3910a8c002b933061a57b9bed5c458bd471
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-09 17:32:49 -0300

    feat(api): add drizzle migration 0005_misty_hammerhead

---
+++ lucrafood-api/drizzle/0005_misty_hammerhead.sql
@@ -0,0 +1,5 @@
+DROP TABLE "accounts" CASCADE;--> statement-breakpoint
+DROP TABLE "ingredient_prices" CASCADE;--> statement-breakpoint
+DROP TABLE "ingredientStores" CASCADE;--> statement-breakpoint
+DROP TABLE "ingredients" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."ingredient_unit";
