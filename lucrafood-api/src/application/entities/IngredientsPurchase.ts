567969d8e2da1becef5729edcf243a2d59c7ed00
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-09 17:33:17 -0300

    feat(api): add src application entities ingredientspurchase

---
+++ lucrafood-api/src/application/entities/IngredientsPurchase.ts
@@ -0,0 +1,49 @@
+
+import { PackageUnit } from '@shared/types/PackageUnit';
+import { randomUUID } from 'node:crypto';
+
+export class IngredientPurchase {
+  readonly id: string;
+  readonly ingredientId: string;
+  readonly storeId?: string | null | undefined;
+
+  readonly packageQty: number;
+  readonly packageUnit: PackageUnit;
+
+  readonly totalPrice: number;
+  readonly unitPrice: number;
+
+  readonly purchasedAt: Date;
+
+  constructor(attrs: IngredientPurchase.Attributes) {
+    this.id = attrs.id ?? randomUUID();
+
+    this.ingredientId = attrs.ingredientId;
+    this.storeId = attrs.storeId;
+
+    this.packageQty = attrs.packageQty;
+    this.packageUnit = attrs.packageUnit;
+
+    this.totalPrice = attrs.totalPrice;
+    this.unitPrice = attrs.unitPrice;
+
+    this.purchasedAt = attrs.purchasedAt ?? new Date();
+  }
+}
+
+export namespace IngredientPurchase {
+  export type Attributes = {
+    id?: string;
+
+    ingredientId: string;
+    storeId?: string | null;
+
+    packageQty: number;
+    packageUnit: PackageUnit;
+
+    totalPrice: number;
+    unitPrice: number;
+
+    purchasedAt?: Date;
+  };
+}
