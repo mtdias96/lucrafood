0034f9afe32db2beab457b6424f7a8b16b7983ab
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-09 17:33:17 -0300

    feat(api): update src application errors http forbidden

--- lucrafood-api/src/application/errors/http/Forbidden.ts
+++ lucrafood-api/src/application/errors/http/Forbidden.ts
@@ -1,15 +1,6 @@
 import { ErrorCode } from '../ErrorCode';
 import { HttpError } from './HttpError';
 
-export class NotFound extends HttpError {
-  public override statusCode = 404;
-  public override code = ErrorCode.NOT_FOUND;
-
-  constructor(message = 'Not Found') {
-    super(message);
-  }
-}
-
 export class Forbidden extends HttpError {
   public override statusCode = 403;
   public override code = ErrorCode.FORBIDDEN;
@@ -19,11 +10,3 @@ export class Forbidden extends HttpError {
   }
 }
 
-export class Conflict extends HttpError {
-  public override statusCode = 409;
-  public override code = ErrorCode.CONFLICT;
-
-  constructor(message = 'Conflict') {
-    super(message);
-  }
-}
