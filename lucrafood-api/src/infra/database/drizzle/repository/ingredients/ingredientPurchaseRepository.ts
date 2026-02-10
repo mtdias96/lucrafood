7cfbfa7cf928ab2dfe40161936874cfa9b080303
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-10 09:12:46 -0300

    feat(api): update ingredient purchase repository operations

--- lucrafood-api/src/infra/database/drizzle/repository/ingredients/ingredientPurchaseRepository.ts
+++ lucrafood-api/src/infra/database/drizzle/repository/ingredients/ingredientPurchaseRepository.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@kernel/decorators/Injactable';
 import { IngredientPurchase } from '@application/entities/IngredientsPurchase';
 import { DbError } from '@application/errors/db/DbError';
 import { DrizzleClient } from '../../Client';
-import { IngredientPriceMapper } from '../../mappers/IngredientPriceMapper';
+
+import { IngredietPurchaseMapper } from '../../mappers/IngredietPurchaseMapper';
 import { ingredientPurchases } from '../../schemas';
 
 @Injectable()
@@ -12,17 +13,17 @@ export class ingredientPurchaseRepository {
   constructor(private readonly db: DrizzleClient) { }
 
   async create(entity: IngredientPurchase): Promise<IngredientPurchase> {
-    const IngredientPurchaseMapper = IngredientPriceMapper.toRow(entity);
+    const values = IngredietPurchaseMapper.toRow(entity);
 
     const [created] = await this.db.client
       .insert(ingredientPurchases)
-      .values(IngredientPurchaseMapper)
+      .values(values)
       .returning();
 
     if (!created) {
       throw new DbError('DB_INSERT_FAILED: ingredientPurchase returned no row');
     }
 
-    return IngredientPriceMapper.toEntity(created);
+    return IngredietPurchaseMapper.toEntity(created);
   }
 }
