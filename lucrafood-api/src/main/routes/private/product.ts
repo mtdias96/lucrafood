9a1e1039b08b1bf24e161c9cc9988dc12a2dc8e0
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-22 15:37:14 -0300

    feat(api): expose endpoint to update product sale price

--- lucrafood-api/src/main/routes/private/product.ts
+++ lucrafood-api/src/main/routes/private/product.ts
@@ -1,6 +1,7 @@
 import { CreateProductController } from '@application/controllers/product/CreateProductController';
 import { CreateProductRecipeController } from '@application/controllers/product/CreateProductRecipeController';
 import { ListAllProductController } from '@application/controllers/product/ListProductsWithRecipeController';
+import { UpdateProductSalePriceController } from '@application/controllers/product/UpdateProductSalePriceController';
 import { Registry } from '@kernel/di/Registry';
 import { fastifyPrivateRouteAdapter } from '@main/adapters/fastifyPrivateRouteAdapter';
 import type { FastifyPluginAsync } from 'fastify';
@@ -8,10 +9,16 @@ import type { FastifyPluginAsync } from 'fastify';
 export const productRoutes: FastifyPluginAsync = async (fastify) => {
   const createProductController = Registry.getInstace().resolve(CreateProductController);
   const createProductRecipeController = Registry.getInstace().resolve(CreateProductRecipeController);
+
   const listProductsWithRecipe = Registry.getInstace().resolve(ListAllProductController);
+
+  const updateProductSalePrice = Registry.getInstace().resolve(UpdateProductSalePriceController);
 
   fastify.get('/products', fastifyPrivateRouteAdapter(listProductsWithRecipe));
 
   fastify.post('/products', fastifyPrivateRouteAdapter(createProductController));
   fastify.post('/products/:productId/recipe', fastifyPrivateRouteAdapter(createProductRecipeController));
+
+  fastify.patch('/products/:productId/sale-price', fastifyPrivateRouteAdapter(updateProductSalePrice));
 };
+
