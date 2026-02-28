2667efd8cd1994cba72faa13d4fa8d0390e041a1
Matheus Dias <matheusdias.front96@gmail.com>
2026-02-28 17:36:15 -0300

    feat(api): expose endpoint to append product recipe items

--- lucrafood-api/src/main/routes/private/product.ts
+++ lucrafood-api/src/main/routes/private/product.ts
@@ -1,10 +1,13 @@
+import type { FastifyPluginAsync } from 'fastify';
+
+import { Registry } from '@kernel/di/Registry';
+import { fastifyPrivateRouteAdapter } from '@main/adapters/fastifyPrivateRouteAdapter';
+
+import { AddProductRecipeController } from '@application/controllers/product/AddProductRecipeController';
 import { CreateProductController } from '@application/controllers/product/CreateProductController';
 import { CreateProductRecipeController } from '@application/controllers/product/CreateProductRecipeController';
 import { ListAllProductController } from '@application/controllers/product/ListProductsWithRecipeController';
 import { UpdateProductSalePriceController } from '@application/controllers/product/UpdateProductSalePriceController';
-import { Registry } from '@kernel/di/Registry';
-import { fastifyPrivateRouteAdapter } from '@main/adapters/fastifyPrivateRouteAdapter';
-import type { FastifyPluginAsync } from 'fastify';
 
 export const productRoutes: FastifyPluginAsync = async (fastify) => {
   const createProductController = Registry.getInstace().resolve(CreateProductController);
@@ -14,10 +17,13 @@ export const productRoutes: FastifyPluginAsync = async (fastify) => {
 
   const updateProductSalePrice = Registry.getInstace().resolve(UpdateProductSalePriceController);
 
+  const addProductRecipeController = Registry.getInstace().resolve(AddProductRecipeController);
+
   fastify.get('/products', fastifyPrivateRouteAdapter(listProductsWithRecipe));
 
   fastify.post('/products', fastifyPrivateRouteAdapter(createProductController));
   fastify.post('/products/:productId/recipe', fastifyPrivateRouteAdapter(createProductRecipeController));
+  fastify.post('/products/:productId/recipe-items', fastifyPrivateRouteAdapter(addProductRecipeController) );
 
   fastify.patch('/products/:productId/sale-price', fastifyPrivateRouteAdapter(updateProductSalePrice));
 };
